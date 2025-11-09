b0a79d3bedd7f10036133fe242459ba9892b383b
anxhukumar <contact.anshukumar@protonmail.com>
2025-11-09 11:41:43 +0530

    add html file to server

--- main.go
+++ main.go
@@ -7,9 +7,13 @@ import (
 
 func main() {
 	// port config
+	const filePathRoot = "."
 	const port = "8080"
 
 	mux := http.NewServeMux()
+
+	// add file config
+	mux.Handle("/", http.FileServer(http.Dir(filePathRoot)))
 
 	serv := &http.Server{
 		Handler: mux,
@@ -17,7 +21,7 @@ func main() {
 	}
 
 	// log confirmation that the port is running
-	log.Printf("Server running on port: %s\n", port)
+	log.Printf("Serving files from %s on port: %s\n", filePathRoot, port)
 
 	// if the server fails it will log the error
 	log.Fatal(serv.ListenAndServe())
