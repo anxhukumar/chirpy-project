c47a91b0736c6a58996c1bf358d14b5fc9470bc7
anxhukumar <contact.anshukumar@protonmail.com>
2025-11-11 13:45:47 +0530

    add state handlers

--- main.go
+++ main.go
@@ -1,9 +1,46 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+// serverHits config
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+// methods of serverHits config
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+// returns current hits
+func (cfg *apiConfig) handlerRequestCount(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	currHits := cfg.fileserverHits.Load()
+	formattedRes := fmt.Sprintf("Hits: %v\n", currHits)
+	w.Write([]byte(formattedRes))
+}
+
+// resets the server hits
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	cfg.fileserverHits.Store(0)
+}
+
+// Non-method handlers
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
 
 func main() {
 	// port config
@@ -12,10 +49,19 @@ func main() {
 
 	mux := http.NewServeMux()
 
-	// add routing
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot))))
-	mux.HandleFunc("/healthz", handlerReadiness)
+	// serverHitsCount state
+	serveHitsCount := apiConfig{}
 
+	// add routing
+	mux.Handle("/app/", http.StripPrefix(
+		"/app",
+		serveHitsCount.middlewareMetricsInc(http.FileServer(http.Dir(filePathRoot))),
+	))
+	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", serveHitsCount.handlerRequestCount)
+	mux.HandleFunc("/reset", serveHitsCount.handlerReset)
+
+	// server struct
 	serv := &http.Server{
 		Handler: mux,
 		Addr:    ":" + port,
@@ -26,10 +72,4 @@ func main() {
 
 	// if the server fails it will log the error
 	log.Fatal(serv.ListenAndServe())
-}
-
-func handlerReadiness(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
